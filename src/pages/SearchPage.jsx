b4422bc08d2cf4f04e7547116c7398eb390bcb34
Connor Tree <Connor>
2023-01-15 10:54:56 +1030

    Added - search page - Final touches

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -6,14 +6,20 @@ import response from "../response";
 import { Link } from "react-router-dom";
 import Search from "../components/Search";
 import SearchIcon from "@mui/icons-material/Search";
-import { Description, Image, LocalOffer, MoreVert, Room } from "@mui/icons-material";
+import {
+  Description,
+  Image,
+  LocalOffer,
+  MoreVert,
+  Room,
+} from "@mui/icons-material";
 
 function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
 
   //LIVE API CALL
-  //const { data } = useGoogleSearch(term);
-  const data = response;
+  const { data } = useGoogleSearch(term);
+  //const data = response;
 
   console.log(data);
   return (
@@ -27,49 +33,91 @@ function SearchPage() {
           />
         </Link>
         <div className="search__page--headerBody">
-          <Search hideButtons/>
+          <Search hideButtons />
           <div className="search__page--options">
             <div className="search__page--optionsLeft">
-
               <div className="search__page--option">
                 <SearchIcon />
-                <Link to="/all">All</Link>
+                <Link to="/search">All</Link>
               </div>
               <div className="search__page--option option__not-allowed">
                 <Description />
-                <Link to="" className="option__not-allowed">News</Link>
+                <Link to="" className="option__not-allowed">
+                  News
+                </Link>
               </div>
               <div className="search__page--option option__not-allowed">
                 <Image />
-                <Link to="" className="option__not-allowed">Images</Link>
+                <Link to="" className="option__not-allowed">
+                  Images
+                </Link>
               </div>
               <div className="search__page--option option__not-allowed">
                 <LocalOffer />
-                <Link to="" className="option__not-allowed">Shopping</Link>
+                <Link to="" className="option__not-allowed">
+                  Shopping
+                </Link>
               </div>
               <div className="search__page--option option__not-allowed">
                 <Room />
-                <Link to="" className="option__not-allowed">Maps</Link>
+                <Link to="" className="option__not-allowed">
+                  Maps
+                </Link>
               </div>
               <div className="search__page--option option__not-allowed">
                 <MoreVert />
-                <Link to="" className="option__not-allowed">More</Link>
+                <Link to="" className="option__not-allowed">
+                  More
+                </Link>
               </div>
-              
             </div>
             <div className="search__page--optionsRight">
               <div className="search__page--option option__not-allowed">
-                <Link to="" className="option__not-allowed">Settings</Link>
+                <Link to="" className="option__not-allowed">
+                  Settings
+                </Link>
               </div>
               <div className="search__page--option option__not-allowed">
-                <Link to="" className="option__not-allowed">Tools</Link>
+                <Link to="" className="option__not-allowed">
+                  Tools
+                </Link>
               </div>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="search__page--results"></div>
+      {term && (
+        <div className="search__page--results">
+          <p className="search__page--resultCount">
+            About {data?.searchInformation.formattedTotalResults} results (
+            {data?.searchInformation.formattedSearchTime} seconds) for {term}
+          </p>
+
+          {data?.items.map((item) => (
+            <div className="search__page--result">
+              <a href={item.link}>
+                {item.pagemap?.cse_image?.length > 0 &&
+                  item.pagemap?.cse_image[0]?.src && (
+                    <img
+                      className="search__page--resultImage"
+                      src={
+                        item.pagemap?.cse_image?.length > 0 &&
+                        item.pagemap?.cse_image[0]?.src
+                      }
+                      alt=""
+                    />
+                  )}
+                {item.displayLink}
+              </a>
+              <a href={item.link} className="search__page--resultTitle">
+                <h2>{item.title}</h2>
+              </a>
+              <p className="search__page--resultSnippet">{item.snippet}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
